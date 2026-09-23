d1b102088301aea0aae1519460795890b84db322
inversevaser <gzgs2025@gmail.com>
2026-09-23 14:21:18 -0400

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,5 +1,5 @@
 from EmotionDetection.emotion_detection import emotion_detector
-import test_emotion_detection
+import unittest
 
 class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
