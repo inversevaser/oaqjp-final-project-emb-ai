4d162ed9462afb0d39649114582e54bd27d9f8d9
Garrett Geisinger <garrett.geisinger.25@cnu.edu>
2026-09-23 22:53:53 -0400

    pylint fixes

--- server.py
+++ server.py
@@ -1,21 +1,25 @@
+""" imports flask and others"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detector")
 
 @app.route("/")
 def index_page():
+    """return with index page"""
     return render_template('index.html')
 
 
 @app.route("/emotionDetector", methods=['GET'])
-def emotionDetector():
+def emotion_detectors():
+    """returning text string from emotion_detector onto the web page"""
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
     final_response = "For the given statement, the system response is "
     final_response += f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
-    final_response += f"'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+    final_response += f"'fear': {response['fear']}, 'joy': {response['joy']} "
+    final_response += f"and 'sadness': {response['sadness']}. "
     final_response += f"The dominant emotion is {response['dominant_emotion']}."
     return final_response
 
