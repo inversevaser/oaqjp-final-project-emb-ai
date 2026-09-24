7611d8ac2358d8d43b690657ebeb0e9fd4c0e3c5
Garrett Geisinger <garrett.geisinger.25@cnu.edu>
2026-09-23 22:41:25 -0400

    error handling available

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,19 +1,26 @@
 import requests
 import json
 
+
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     obj = {"raw_document": {"text": text_to_analyze}}
 
-    ret_resp = requests.post(url, json = obj, headers=header)
-    format_resp = json.loads(ret_resp.text)
+    ret_resp = requests.post(url, json=obj, headers=header)
+    try:
 
-    emotions_dict = format_resp["emotionPredictions"][0]["emotion"]
+        format_resp = json.loads(ret_resp.text)
 
-    dominant_emotion_num = max(emotions_dict.values())
+        emotions_dict = format_resp["emotionPredictions"][0]["emotion"]
 
-    for key in list(emotions_dict.keys()):
-        if dominant_emotion_num == emotions_dict[key]:
-            emotions_dict["dominant_emotion"] = key
+        dominant_emotion_num = max(emotions_dict.values())
+
+        for key in list(emotions_dict.keys()):
+            if dominant_emotion_num == emotions_dict[key]:
+                emotions_dict["dominant_emotion"] = key
+    except:
+        if ret_resp.status_code == "400" or "500":
+            return {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None,
+                    "dominant_emotion": None}
     return emotions_dict
--- server.py
+++ server.py
@@ -9,15 +9,16 @@ def index_page():
 
 @app.route("/emotionDetector", methods=['GET'])
 def emotionDetector():
-    text_analyzee = request.args.get("emotionPredictions")
-
+    text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
-
+    if response['dominant_emotion'] == None:
+        return "Invalid text! Please try again!"
     final_response = "For the given statement, the system response is "
     final_response += f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
-    final_response += f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+    final_response += f"'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. "
     final_response += f"The dominant emotion is {response['dominant_emotion']}."
     return final_response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
